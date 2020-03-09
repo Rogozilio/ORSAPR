552513be558cbac2fb289f465e9c787a0a0a370f
Руслан Рогозин <wether@mail2000.ru>
2020-03-09 18:52:53 +0700

    первый коммит

--- ORSAPR/model/Exceptions/ValueOutOfRangeException.cs
+++ ORSAPR/model/Exceptions/ValueOutOfRangeException.cs
@@ -5,7 +5,7 @@ namespace ORSAPR.model.Exceptions
     public class ValueOutOfRangeException : Exception
     {
         public ValueOutOfRangeException(string nameValue, int min, int max)
-            : base(nameValue+" находится вне диапазона допустимых значений "+min+" - "+max) 
+            : base(nameValue+" находится вне диапазона допустимых значений "+min+" - "+max)
         {
         }
     }
